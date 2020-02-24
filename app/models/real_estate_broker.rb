eed9e0436404c92d3633eabebdd162ad868be3f8
patito <p.sotosellanes@hotmail.com>
2020-02-24 20:49:04 -0300

    aws

--- app/models/real_estate_broker.rb
+++ app/models/real_estate_broker.rb
@@ -1,2 +1,4 @@
 class RealEstateBroker < ApplicationRecord
+    has_many :opinions
+    has_many :users, through: :opinions
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :opinions
+  has_many :real_estate_brokers, through: :opinions       
 end
