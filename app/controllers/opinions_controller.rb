d81800dd0b61b9d17a21cb4fc1f5cf2a093e3d83
patito <p.sotosellanes@hotmail.com>
2020-02-25 08:46:00 -0300

    opinions controller

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,2 +1,54 @@
 class OpinionsController < ApplicationController
+    before_action :set_opinion, only: [:show, :edit, :update, :destroy]
+    before_action :set_real_estate_broker
+    before_action :authenticate_user!
+
+    def new
+        @opinion = Opinion.new
+    end    
+
+    def edit    
+    end    
+
+    def create
+        @opinion = Opinion.new(opinion_params)
+        @opinion.user_id = current_user.user_id
+        @opinion.real_estate_broker_id = @opinion.user_id
+        if @opinion.save
+            redirect_to @real_estate_broker_id
+        else    
+            render 'new'
+        end
+    end  
+    
+    def update
+        respond_to do |format|
+            if @opinion.update(opinion_params)
+                format.html { redirect_to @opinion, notice: 'Tu comentario se ha actualizado.' }
+            else
+                format.html { render :edit }   
+            end
+        end
+    end  
+    
+    def destroy
+        @opinion.destroy
+        respond_to do |format|
+            format.html { redirect_to opinions_url, notice: 'Tu comentario se ha eliminado!.' }
+        end
+    end
+    
+    private
+
+        def set_opinion
+            @opinion = Opinion.fin(params[:id])
+        end
+        
+        def set_real_estate_broker
+            @real_estate_broker = Real_estate_broker.find(params[:real_estate_broker_id])
+        end    
+        
+        def opinion_params
+            params.require(:opinion).permit(:rating, :comment)
+        end    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :real_estate_brokers
   devise_for :users
+  
+  resources :real_estate_brokers do
+      resources :opinions, except: [:index, :show]
+  end
   get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
