59b6d8596306fe35ba2a83c46a8ad04c9c63fc3a
patito <p.sotosellanes@hotmail.com>
2020-02-29 23:14:44 -0300

    opinions form

--- app/models/real_estate_broker.rb
+++ app/models/real_estate_broker.rb
@@ -1,5 +1,5 @@
 class RealEstateBroker < ApplicationRecord
-    has_many :opinions
+    has_many :opinions, dependent: :destroy
     belongs_to :user 
     validates_uniqueness_of :rut
     has_one_attached :image
