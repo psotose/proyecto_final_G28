669321fda28ae4f61540f2ae497d154778d7eb54
patito <p.sotosellanes@hotmail.com>
2020-03-01 12:51:21 -0300

    reb default image

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+10.times do |i|
+    u = User.create(name: "User #{i}",email: "#{i}@gmail.com", password: "123456")
+    p = RealEstateBroker.create(title: "Corredora #{i}", content: "Contenido #{i}", user: u )
+    10.times do |j|
+      c = p.opinions.build(content: "Comentario #{j} del post #{i}", user: u, real_estate_broker: p).save!
+    end
+  end
