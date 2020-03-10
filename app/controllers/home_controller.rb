b7cce4349539689a54ebf1a477ed942b927a70a7
patito <p.sotosellanes@hotmail.com>
2020-03-10 00:25:05 -0300

    kaminari gem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,9 @@ class HomeController < ApplicationController
   
   def index
     @real_estate_brokers = unless params[:q]
-      RealEstateBroker.all
+      RealEstateBroker.page(params[:page])
     else      
-      RealEstateBroker.where('name ILIKE ?', "%#{params[:q]}%") 
+      RealEstateBroker.page(params[:page]).where('name ILIKE ?', "%#{params[:q]}%") 
     end 
   end                        
 end
