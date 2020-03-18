d8025855667bf2a8e08eaa2448553ccf2e087cc4
patito <p.sotosellanes@hotmail.com>
2020-03-18 18:10:55 -0300

    some bootstrap

--- app/controllers/real_estate_brokers_controller.rb
+++ app/controllers/real_estate_brokers_controller.rb
@@ -1,6 +1,6 @@
 class RealEstateBrokersController < ApplicationController
   before_action :set_real_estate_broker, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:show]
 
   # GET /real_estate_brokers
   # GET /real_estate_brokers.json
