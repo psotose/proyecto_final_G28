907465a16377ac63db266a37171b97ee06d28357
patito <p.sotosellanes@hotmail.com>
2020-03-19 15:19:36 -0300

    try

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,10 @@ class HomeController < ApplicationController
   
   def index
     @real_estate_brokers = unless params[:q]
-      RealEstateBroker.all
+      RealEstateBroker.page(params[:page])
     else      
       RealEstateBroker.where('name ILIKE ?', "%#{params[:q]}%") 
     end 
-    @real_estate_brokers = @real_estate_brokers.order_by_opinion
+   
   end                        
 end
